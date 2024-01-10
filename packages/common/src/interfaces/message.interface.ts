b628b5d2ad17816d6d22a300e8fb81e7392ffe6e
codytseng <codytseng98@gmail.com>
2024-01-10 22:48:22 +0800

    feat: enhance TypeScript typing for MessageType

--- packages/common/src/interfaces/message.interface.ts
+++ packages/common/src/interfaces/message.interface.ts
@@ -3,16 +3,24 @@ import { EventId, SubscriptionId } from './common.interface';
 import { Event } from './event.interface';
 import { Filter } from './filter.interface';
 
+type MessageTypeEvent = MessageType.EVENT | 'EVENT';
+type MessageTypeReq = MessageType.REQ | 'REQ';
+type MessageTypeClose = MessageType.CLOSE | 'CLOSE';
+type MessageTypeAuth = MessageType.AUTH | 'AUTH';
+type MessageTypeOk = MessageType.OK | 'OK';
+type MessageTypeEose = MessageType.EOSE | 'EOSE';
+type MessageTypeNotice = MessageType.NOTICE | 'NOTICE';
+
 export type IncomingMessage =
   | IncomingEventMessage
   | IncomingReqMessage
   | IncomingCloseMessage
   | IncomingAuthMessage;
 
-export type IncomingEventMessage = [MessageType.EVENT, Event];
-export type IncomingReqMessage = [MessageType.REQ, SubscriptionId, ...Filter[]];
-export type IncomingCloseMessage = [MessageType.CLOSE, SubscriptionId];
-export type IncomingAuthMessage = [MessageType.AUTH, Event];
+export type IncomingEventMessage = [MessageTypeEvent, Event];
+export type IncomingReqMessage = [MessageTypeReq, SubscriptionId, ...Filter[]];
+export type IncomingCloseMessage = [MessageTypeClose, SubscriptionId];
+export type IncomingAuthMessage = [MessageTypeAuth, Event];
 
 export type OutgoingMessage =
   | OutgoingOkMessage
@@ -21,8 +29,8 @@ export type OutgoingMessage =
   | OutgoingNoticeMessage
   | OutgoingAuthMessage;
 
-export type OutgoingOkMessage = [MessageType.OK, EventId, boolean, string];
-export type OutgoingEventMessage = [MessageType.EVENT, SubscriptionId, Event];
-export type OutgoingEoseMessage = [MessageType.EOSE, SubscriptionId];
-export type OutgoingNoticeMessage = [MessageType.NOTICE, string];
-export type OutgoingAuthMessage = [MessageType.AUTH, string];
+export type OutgoingOkMessage = [MessageTypeOk, EventId, boolean, string];
+export type OutgoingEventMessage = [MessageTypeEvent, SubscriptionId, Event];
+export type OutgoingEoseMessage = [MessageTypeEose, SubscriptionId];
+export type OutgoingNoticeMessage = [MessageTypeNotice, string];
+export type OutgoingAuthMessage = [MessageTypeAuth, string];
